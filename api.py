b68990c31c46049992c2d6bc1f102152d3d528b4
papichoolo <souravsahil100@gmail.com>
2026-03-02 18:35:29 +0530

    some more minor fixes

--- api.py
+++ api.py
@@ -608,4 +608,5 @@ def trigger_etl(x_etl_secret: Optional[str] = Header(None, alias="X-ETL-Secret")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
